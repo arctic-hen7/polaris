24c35a796170cbfdcd15a797bfc408d58142f880
arctic-hen7 <arctic.hen@pm.me>
2025-04-10 11:30:01 +1000

    fix: removed effort fallback

--- src/parse/action_item.rs
+++ src/parse/action_item.rs
@@ -264,8 +264,7 @@ impl Effort {
             Some("minimal") => Ok(Self::Minimal),
             Some("min") => Ok(Self::Minimal),
             Some(e) => bail!("unknown effort '{e}' on node {}", node.id),
-            None => Ok(Self::Medium),
-            // None => bail!("no effort level specified for node {}", node.id),
+            None => bail!("no effort level specified for node {}", node.id),
         }
     }
 
