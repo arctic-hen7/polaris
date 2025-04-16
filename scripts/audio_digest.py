bb50ce7a9ca4da1361b50495ba2bf46d1b9350b2
arctic-hen7 <arctic.hen@pm.me>
2025-04-16 14:50:28 +1000

    fix: migrated back to `tts-1` for daily digest
    Personally not a fan of `gpt-4o-mini-tts` just yet...

--- scripts/audio_digest.py
+++ scripts/audio_digest.py
@@ -3,7 +3,8 @@
 # to convert the digest into a script first, and then speaking it aloud using the `tts`
 # API.
 
-DIGEST_SCRIPT_INSTRUCTIONS = "You are a fun assistant in part of a pipeline to deliver a spoken daily digest to me, a founder. You will be given the raw Markdown of a daily digest file containing events, daily notes (i.e. things to remember), goals for the day, week, and general goals that are shown every day, and urgent actions that need to be done during the day (which might include problems/projects to be worked on). You should provide a script version of this that can be spoken fluently by a text-to-speech engine. Make sure to include all the detail of the daily digest and not change anything, just reformat it so it can be spoken fluently. You should open with a cheerful \"Good morning\" or similar, and close with a positive message to have a great day."
+DIGEST_SCRIPT_INSTRUCTIONS = "You are a fun assistant in part of a pipeline to deliver a spoken daily digest to me, a founder and student. You will be given the raw Markdown of a daily digest file containing events, daily notes (i.e. things to remember), goals for the day, week, and general goals that are shown every day, and urgent actions that need to be done during the day (which might include problems/projects to be worked on). You should provide a script version of this that can be spoken fluently by a text-to-speech engine. Make sure to include all the detail of the daily digest and not change anything, just reformat it so it can be spoken fluently. You should open with a cheerful \"Good morning\" or similar, and close with a positive message to have a great day. Otherwise, don't add too many embellishments."
+AUDIO_INSTRUCTIONS = "Speak in a cheerful and upbeat tone."
 TTS_VOICE = "nova"
 
 if __name__ == "__main__":
@@ -27,8 +28,9 @@ if __name__ == "__main__":
 
     print("Generating digest audio...")
     with client.audio.speech.with_streaming_response.create(
-        model="gpt-4o-mini-tts",
+        model="tts-1",
         voice=TTS_VOICE,
-        input=response.output_text
+        input=response.output_text,
+        # instructions=AUDIO_INSTRUCTIONS,
     ) as response:
         response.stream_to_file(audio_file_path)
